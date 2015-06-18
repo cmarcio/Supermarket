6edb0c046578f688a1dc3cda68943220086e9fa7
marciodscampos <marciodscampos@gmail.com>
2015-06-18 02:58:24 -0300

    Janela inicial adicionada

--- Client/src/gui/Controller.java
+++ Client/src/gui/Controller.java
@@ -1,4 +1,52 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    // Janela inicial
+    @FXML private Button btnLogin;
+    @FXML private Button btnRegister;
+
+    // Abre uma janela de acordo com o botão clicado
+    @FXML void handleStartWindow(ActionEvent event) {
+        Button source = (Button) event.getSource();
+        if (source == btnLogin) {
+        }
+        else {
+            loadWindow(source, "CadastrarUsuario_cliente.fxml");
+        }
+    }
+
+    // Exibe uma mensagem contendo as strings passadas como parâmetro, e o tipo especificado
+    public void showMessage(String title, String header, String message, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // Carrega uma janela
+    public void loadWindow(Button btn, String fxml) {
+        Stage stage=(Stage) btn.getScene().getWindow();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(fxml));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("ERROR LOADING WINDOW!");
+            e.printStackTrace();
+        }
+    }
 }
--- Client/src/gui/Main.java
+++ Client/src/gui/Main.java
@@ -10,12 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Inicio_cliente.fxml"));
+        primaryStage.setTitle("Cliente");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
